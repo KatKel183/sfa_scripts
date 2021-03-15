10cecb457096cd9a637bb6ee064f575f09dbe345
KatKel183 <79229793+KatKel183@users.noreply.github.com>
2021-03-15 14:34:14 -0500

    has a SimpleUI class that sets Window Title.

---
+++ src/simpleui.py
@@ -0,0 +1,11 @@
+from Pyside2 import QtWidgets
+
+
+class SimpleUI(QtWidgets.QDialog):
+    """This is my SimpleUI class to create graphical user interface"""
+    def __init__(self):
+        """Constructor"""
+        # Passing the object SimpleUI as an argument to super()
+        # makes this line python 2 and 3 compatible
+        super(SimpleUI, self).__init__()
+        self.setWindowTitle("A Simple UI")
