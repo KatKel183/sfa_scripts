cacfff6789acb12151d1c792b8d52debf8347c78
KatKel183 <79229793+KatKel183@users.noreply.github.com>
2021-04-14 19:51:14 -0500

    Rewrote code in GUI classes so preliminary titles and boxes showed up properly

--- src/scatter.py
+++ src/scatter.py
@@ -27,16 +27,57 @@ class ScatterUI(QtWidgets.QDialog):
         self.setMaximumHeight(350)
         self.setWindowFlags(self.windowFlags() ^
                             QtCore.Qt.WindowContextHelpButtonHint)
-        # load the logic class here
+        self.scatter = Scatter()
         self.create_ui()
+        # self.create_connections()
 
     def create_ui(self):
         self.title_lbl = QtWidgets.QLabel("Scatter")
         self.title_lbl.setStyleSheet("font: bold 35px")
+        # folder layout line
+        self.transform_lay = self._create_transform_ui()
+        # button layout line
         self.main_lay = QtWidgets.QVBoxLayout()
         self.main_lay.addWidget(self.title_lbl)
+        # addLayout for folder layout
+        self.main_lay.addLayout(self.transform_lay)
+        self.main_lay.addStretch()
+        # addLayout button
         self.setLayout(self.main_lay)
 
+    # def create_connections(self):
+    #    """Connect our widget signals to slots"""
+
+    def _create_transform_ui(self):
+        layout = self._create_transform_headers()
+        self.scatter_object_le = QtWidgets.QLineEdit(
+            self.scatter.scatter_object)
+        self.scatter_object_le.setMinimumWidth(100)
+        self.destination_object_le = QtWidgets.QLineEdit(
+            self.scatter.destination_object)
+        self.destination_object_le.setMinimumWidth(100)
+        self.rx_max_sbx = QtWidgets.QSpinBox()
+        self.rx_max_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.rx_max_sbx.setFixedWidth(100)
+        # might need to put more code in here
+        layout.addWidget(QtWidgets.QLabel("Rotate"), 1, 0)
+        layout.addWidget(self.scatter_object_le, 2, 0)
+        # definitely need to put a ton more code here
+        return (layout)
+
+    def _create_transform_headers(self):
+        self.scatter_header_lbl = QtWidgets.QLabel("Choose Scatter Object: ")
+        self.scatter_header_lbl.setStyleSheet("font:bold")
+        self.destination_header_lbl = QtWidgets.QLabel(
+            "Choose Object/Vertices to Scatter To")
+        self.destination_header_lbl.setStyleSheet("font:bold")
+        self.rotate_header_lbl = QtWidgets.QLabel("Rotate")
+        self.rotate_header_lbl.setStyleSheet("font: bold")
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.scatter_header_lbl, 0, 0)
+        layout.addWidget(self.destination_header_lbl, 0, 2)
+        layout.addWidget(self.rotate_header_lbl, 0, 4)
+        return (layout)
 
 # CLASS: create logic for program
 class Scatter(object):
@@ -44,71 +85,64 @@ class Scatter(object):
 
     #the init has the defaults
     def __init__(self):
-        self.r_min_x = 0
-        self.r_max_x = 360
-        self.r_min_y = 0
-        self.r_max_y = 360
-        self.r_min_z = 0
-        self.r_max_z = 360
+        # user_input of shape to scatter
+        self.scatter_object = 'pCube1'
+        # when is init called?
+        self.destination_object = self.vert_selection()
+        # call the stuff for rotations and scale
 
-    #def __init__(self):
-    # variable to store user-input of shape to scatter (scatter-object).
 
-    # scatter_object = QLineEdit.displayText()
+    def transform_adjust(self):
+        # these should start out with un-transformed values
+        sca_min_x = 1.0
+        sca_max_x = 1.0
+        sca_min_y = 1.0
+        sca_max_y = 1.0
+        sca_min_z = 1.0
+        sca_max_z = 1.0
+        # rotation is 0 to 360.
+        r_min_x = 0
+        r_max_x = 0
+        r_min_y = 0
+        r_max_y = 0
+        r_min_z = 0
+        r_max_z = 0
 
-    # variable to store verts/object that user selects (scatter-to)
+    def transform_random(self):
+        # scale variables
+        size_x = random.uniform(1.0, 5.5)
+        size_y = random.uniform(1.0, 5.5)
+        size_z = random.uniform(1.0, 5.5)
 
-    # Loop code -- Allows scatter-object to scatter to selected verts only.
-    # selection = cmds.ls(orderedSelection=True, flatten=True)
+        # rotate variables - replace hard-coded with user-input variables
+        rotate_x = random.uniform(0, 360)
+        rotate_y = random.uniform(0, 360)
+        rotate_z = random.uniform(0, 360)
 
-    # for obj in selection:
-        # if 'vtx[' not in obj:
-            # selection.remove(obj)
-
-    # --Selecting locations for scatter-object to scatter to
-    selection = cmds.ls(orderedSelection=True, flatten=True)
-    # lines 38-9 select all verts on an object:
-    vtx_selection = cmds.polyListComponentConversion(selection, toVertex=True)
-    vtx_selection = cmds.filterExpand(vtx_selection, selectionMask=31,
+    def vert_selection(self):
+        # --Selecting locations for scatter-object to scatter to
+        selection = cmds.ls(orderedSelection=True, flatten=True)
+        # select all verts on an object:
+        vtx_selection = cmds.polyListComponentConversion(selection,
+                                                         toVertex=True)
+        vtx_selection = cmds.filterExpand(vtx_selection, selectionMask=31,
                                       expand=True)
-    # line 40 selects or re-selects the vertices previously selected
-    cmds.select(vtx_selection)
-
-    # -- selecting scale for scatter-object instances - how many variables?
-    # scale_min = user_input
-    # scale_max = user_input
-
-    # scale variables
-    size_x = random.uniform(1.0, 5.5)
-    size_y = random.uniform(1.0, 5.5)
-    size_z = random.uniform(1.0, 5.5)
+        # selects or re-selects the vertices previously selected
+        cmds.select(vtx_selection)
+        return vtx_selection
 
 
-    # -- selecting rotation for scatter-object instances
-    # ro_min_X = user_input
-    # ro_max_X = user_input
-    # ro_min_Y = user_input
-    # ro_max_Y = user_input
-    # ro_min_Z = user_input
-    # ro_max_Z = user_input
 
-    # rotate variables - replace hard-coded with user-input variables
-    rotate_x = random.uniform(0, 360)
-    rotate_y = random.uniform(0, 360)
-    rotate_z = random.uniform(0, 360)
-
-    # code that groups the instances
-    # empty list so instances can be grouped
-    scattered_instances = []
-    # this for loop creates instances, stores in list
-    for vtx in vtx_selection:
+    def create_instances(self):
+        scattered_instances = []
+        for vtx in vert_selection():
         # change pCube1 to soft-coded variable - scatter_object
-        # I think "instance#" is causing problems--# is special python
-        scatter_instance = cmds.instance("pCube1", name="instance#")
-        scattered_instances.extend(scatter_instance)
-        pos = cmds.xform([vtx], query=True, translation=True, worldSpace=True)
-        cmds.xform(scatter_instance, translation=pos,
-                   rotation=[rotate_x, rotate_y, rotate_z],
-                   scale=[size_x, size_y, size_z])
+        #scatter_instance = cmds.instance("pCube1", name="instance#")
+            scatter_instance = cmds.instance("pCube1", name="instance1")
+            scattered_instances.extend(scatter_instance)
+            pos = cmds.xform([vtx], query=True, translation=True, worldSpace=True)
+            cmds.xform(scatter_instance, translation=pos,
+                    rotation=[rotate_x, rotate_y, rotate_z],
+                    scale=[size_x, size_y, size_z])
     # group the instances -- arguments = list and a name for the list
-    cmds.group(scattered_instances, name="scattered")
+        cmds.group(scattered_instances, name="scattered")
