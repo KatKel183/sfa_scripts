aabab1104e0ee3cedef2c926508eadfc884897a6
KatKel183 <79229793+KatKel183@users.noreply.github.com>
2021-04-23 14:49:53 -0500

    Changes based on April 19 Workshop include addition of lists to store transformation values

--- src/scatter.py
+++ src/scatter.py
@@ -9,19 +9,18 @@ from pymel.core.system import Path
 
 # log = logging.getLogger(__name__)
 
-# make sure the window appears above the maya window
+
 def maya_main_window():
     """Return the maya main window widget"""
     main_window = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window), QtWidgets.QWidget)
 
-# CLASS: create the GUI
+
 class ScatterUI(QtWidgets.QDialog):
-    """ Smart Save UI Class"""
 
     def __init__(self):
         super(ScatterUI, self).__init__(parent=maya_main_window())
-        self.setWindowTitle("Scatter")
+        self.setWindowTitle("Scatter Tool")
         # The pixel settings are adjusted for my computer resolution
         self.setMinimumWidth(750)
         self.setMaximumHeight(350)
@@ -29,37 +28,43 @@ class ScatterUI(QtWidgets.QDialog):
                             QtCore.Qt.WindowContextHelpButtonHint)
         self.scatter = Scatter()
         self.create_ui()
-        # self.create_connections()
+        self.create_connections()
 
     def create_ui(self):
+        # vertical box layout
+        self.main_layout = QtWidgets.QVBoxLayout
+        self.scatter_btn = QtWidgets.QPushButton("Scatter Tool")
+        # visuals for the rotation boxes
+        self.rotation_y_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.rotation_y_min_dsbx.setMaximum(360)
+        self.rotation_y_max_dsbx = QtWidgets.QDoubleSpinBox()
+        self.rotation_y_max_dsbx.setMaximum(360)
+        # -----what I coded
         self.title_lbl = QtWidgets.QLabel("Scatter")
         self.title_lbl.setStyleSheet("font: bold 35px")
-        # folder layout line
         self.transform_lay = self._create_transform_ui()
         self.button_lay = self._create_button_ui()
         self.main_lay = QtWidgets.QVBoxLayout()
         self.main_lay.addWidget(self.title_lbl)
-        # addLayout for folder layout
         self.main_lay.addLayout(self.transform_lay)
         self.main_lay.addStretch()
         self.main_lay.addLayout(self.button_lay)
         self.setLayout(self.main_lay)
 
+
     def create_connections(self):
-        """Connect our widget signals to slots"""
-        self.scatter_btn.clicked.connect(self._scatter)
+        self.scatter_btn.clicked.connect(self._scatter_slot)
 
     @QtCore.Slot()
-    def _scatter(self):
+    def _scatter_slot(self):
         """Scatter objects onto vertices"""
         self._set_scatter_properties_from_ui()
-        self.scenefile.create_instances()
+        self._set_values_from_ui()
+        self.scatter.create_instances()
 
     def _set_scatter_properties_from_ui(self):
         self.scatter.scatter_object = self.scatter_object_le.text()
         self.scatter.destination_object = self.destination_object_le.text()
-        # code for rotate and scale
-        # self.scenefile.rx_min = self.??.value()
 
     def _create_button_ui(self):
         self.scatter_btn = QtWidgets.QPushButton("Scatter Objects")
@@ -72,7 +77,8 @@ class ScatterUI(QtWidgets.QDialog):
         self.scatter_object_le = QtWidgets.QLineEdit(
             self.scatter.scatter_object)
         self.scatter_object_le.setMinimumWidth(100)
-        self.destination_object_le = QtWidgets.QLineEdit(
+        # QtWidgets.QLineEdit vs QtWidgets.QListWidget
+        self.destination_object_le = QtWidgets.QListWidget(
             self.scatter.destination_object)
         self.destination_object_le.setMinimumWidth(100)
         # transform spinboxes now in two separate methods
@@ -96,6 +102,13 @@ class ScatterUI(QtWidgets.QDialog):
         layout.addWidget(QtWidgets.QLabel("ScaleY"), 2, 5)
         layout.addWidget(QtWidgets.QLabel("ScaleZ"), 2, 6)
         # user input for max
+        layout.addWidget(self.rx_min_dsbx, 3, 1)
+        layout.addWidget(self.ry_min_dsbx, 3, 2)
+        layout.addWidget(self.rz_min_dsbx, 3, 3)
+        layout.addWidget(self.sx_min_dsbx, 3, 4)
+        layout.addWidget(self.sy_min_dsbx, 3, 5)
+        layout.addWidget(self.sz_min_dsbx, 3, 6)
+        # user input for max
         layout.addWidget(self.rx_max_dsbx, 4, 1)
         layout.addWidget(self.ry_max_dsbx, 4, 2)
         layout.addWidget(self.rz_max_dsbx, 4, 3)
@@ -105,17 +118,29 @@ class ScatterUI(QtWidgets.QDialog):
         return layout
 
     def scale_sbxes(self):
-        # settings for sx spinboxes
+        # sx min and max
+        self.sx_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.sx_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.sx_min_dsbx.setFixedWidth(100)
         self.sx_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.sx_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
         self.sx_max_dsbx.setFixedWidth(100)
-        # settings for sy spinboxes
+        # sy min and max
+        self.sy_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.sy_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.sy_min_dsbx.setFixedWidth(100)
         self.sy_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.sy_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
         self.sy_max_dsbx.setFixedWidth(100)
-        # settings for sz spinboxes
+        # sz min and max
+        self.sz_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.sz_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.sz_min_dsbx.setFixedWidth(100)
         self.sz_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.sz_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
@@ -123,16 +148,28 @@ class ScatterUI(QtWidgets.QDialog):
 
     def rotate_sbxes(self):
         # settings for rx spinboxes
+        self.rx_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.rx_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.rx_min_dsbx.setFixedWidth(100)
         self.rx_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.rx_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
         self.rx_max_dsbx.setFixedWidth(100)
         # settings for ry spinboxes
+        self.ry_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.ry_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.ry_min_dsbx.setFixedWidth(100)
         self.ry_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.ry_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
         self.ry_max_dsbx.setFixedWidth(100)
         # settings for rz spinboxes
+        self.rz_min_dsbx = QtWidgets.QDoubleSpinBox()
+        self.rz_min_dsbx.setButtonSymbols(
+            QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.rz_min_dsbx.setFixedWidth(100)
         self.rz_max_dsbx = QtWidgets.QDoubleSpinBox()
         self.rz_max_dsbx.setButtonSymbols(
             QtWidgets.QAbstractSpinBox.PlusMinus)
@@ -155,45 +192,25 @@ class ScatterUI(QtWidgets.QDialog):
         layout.addWidget(self.scale_header_lbl, 1, 5)
         return (layout)
 
-# CLASS: create logic for program
+
 class Scatter(object):
-    """Representation of a Scatter program"""
 
-    #the init has the defaults
     def __init__(self):
-        # user_input of shape to scatter
+        self.rotation_min = [0, 0, 0]
+        self.rotation_max = [360, 360, 360]
+        self.scale_min = [1, 1, 1]
+        self.scale_max = [20, 20, 20]
+        # source and destination
         self.scatter_object = 'pCube1'
-        # when is init called?
         self.destination_object = self.vert_selection()
-        # call the stuff for rotations and scale
-
-
-    def scale_and_rotate(self):
-        # these should start out with un-transformed values
-        sca_min_x = 1.0
-        sca_max_x = 1.0
-        sca_min_y = 1.0
-        sca_max_y = 1.0
-        sca_min_z = 1.0
-        sca_max_z = 1.0
-        # rotation is 0 to 360.
-        r_min_x = 0
-        r_max_x = 0
-        r_min_y = 0
-        r_max_y = 0
-        r_min_z = 0
-        r_max_z = 0
 
     def transform_random(self):
-        # scale variables - replace hard-coded with user-input variables
-        size_x = random.uniform(1.0, 5.5)
-        size_y = random.uniform(1.0, 5.5)
-        size_z = random.uniform(1.0, 5.5)
-
-        # rotate variables - replace hard-coded with user-input variables
-        rotate_x = random.uniform(0, 360)
-        rotate_y = random.uniform(0, 360)
-        rotate_z = random.uniform(0, 360)
+        self.size_x = random.uniform(sx_min, sx_max)
+        self.size_y = random.uniform(sy_min, sy_max)
+        self.size_z = random.uniform(sz_min, sz_max)
+        self.rotate_x = random.uniform(rx_min, rx_max)
+        self.rotate_y = random.uniform(ry_min, ry_max)
+        self.rotate_z = random.uniform(rz_min, rz_max)
 
     def vert_selection(self):
         # --Selecting locations for scatter-object to scatter to
@@ -207,18 +224,15 @@ class Scatter(object):
         cmds.select(vtx_selection)
         return vtx_selection
 
-
-
     def create_instances(self):
         scattered_instances = []
-        for vtx in vert_selection():
-        # change pCube1 to soft-coded variable - scatter_object
-        #scatter_instance = cmds.instance("pCube1", name="instance#")
-            scatter_instance = cmds.instance("pCube1", name="instance1")
+        for vtx in self.vert_selection():
+            pos = cmds.pointPosition(vtx)
+            scatter_instance = cmds.instance(self.scatter_object,
+                                             name="scatter_1")
             scattered_instances.extend(scatter_instance)
-            pos = cmds.xform([vtx], query=True, translation=True, worldSpace=True)
-            cmds.xform(scatter_instance, translation=pos,
-                    rotation=[rotate_x, rotate_y, rotate_z],
-                    scale=[size_x, size_y, size_z])
+            cmds.move(pos[0], pos[1], pos[2], self.scatter_object,
+                      worldSpace=True)
+
     # group the instances -- arguments = list and a name for the list
         cmds.group(scattered_instances, name="scattered")
